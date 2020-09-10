bf8310f832139d1569068d51e372d26cc3983ed0
Nicholas Dallas <nicholas.dallas0@gmail.com>
2020-09-09 19:49:01 -0700

    corrected workout.js model file to include workout

--- models/Workout.js
+++ models/Workout.js
@@ -1,130 +1,147 @@
-const { model, schema } = require('mongoose')
+const { model, schema, Schema } = require('mongoose')
 
 const Workout = new Schema ({
-    day: new Date().setDate(new Date().getDate()-10),
-    exercises: [
-      {
-        type: "resistance",
-        name: "Bicep Curl",
-        duration: 20,
-        weight: 100,
-        reps: 10,
-        sets: 4
-      }
-    ]
-  },
-  {
-    day: new Date().setDate(new Date().getDate()-9),
-    exercises: [
-      {
-        type: "resistance",
-        name: "Lateral Pull",
-        duration: 20,
-        weight: 300,
-        reps: 10,
-        sets: 4
-      }
-    ]
-  },
-  {
-    day: new Date().setDate(new Date().getDate()-8),
-    exercises: [
-      {
-        type: "resistance",
-        name: "Push Press",
-        duration: 25,
-        weight: 185,
-        reps: 8,
-        sets: 4
-      }
-    ]
-  },
-  {
-    day: new Date().setDate(new Date().getDate()-7),
-    exercises: [
-      {
-        type: "cardio",
-        name: "Running",
-        duration: 25,
-        distance: 4
-      }
-    ]
-  },
-  {
-    day: new Date().setDate(new Date().getDate()-6),
-    exercises: [
-      {
-        type: "resistance",
-        name: "Bench Press",
-        duration: 20,
-        weight: 285,
-        reps: 10,
-        sets: 4
-      }
-    ]
-  },
-  {
-    day: new Date().setDate(new Date().getDate()-5),
-    exercises: [
-      {
-        type: "resistance",
-        name: "Bench Press",
-        duration: 20,
-        weight: 300,
-        reps: 10,
-        sets: 4
-      }
-    ]
-  },
-  {
-    day: new Date().setDate(new Date().getDate()-4),
-    exercises: [
-      {
-        type: "resistance",
-        name: "Quad Press",
-        duration: 30,
-        weight: 300,
-        reps: 10,
-        sets: 4
-      }
-    ]
-  },
-  {
-    day: new Date().setDate(new Date().getDate()-3),
-    exercises: [
-      {
-        type: "resistance",
-        name: "Bench Press",
-        duration: 20,
-        weight: 300,
-        reps: 10,
-        sets: 4
-      }
-    ]
-  },
-  {
-    day: new Date().setDate(new Date().getDate()-2),
-    exercises: [
-      {
-        type: "resistance",
-        name: "Military Press",
-        duration: 20,
-        weight: 300,
-        reps: 10,
-        sets: 4
-      }
-    ]
-  },
-  {
-    day: new Date().setDate(new Date().getDate()-1),
-    exercises: [
-      {
-        type: "resistance",
-        name: "Bench",
-        duration: 30,
-        distance: 2
-      }
-    ]
-  }, { timestamps: true })
+    text: {
+        type: String,
+        name: 'Not Null',
+        duration: 'parseInt',
+        weight: 'parseInt',
+        reps: 'parseInt',
+        sets: 'parseInt',
+        unique: true,
+        required: true
+    },
+    user: {
+        type: Schema.Types.ObjectId,
+        ref: 'User'
+    }
+ }, { timestamps: true })
 
   module.exports = model('Workout', Workout)
+
+//   {
+//     day: new Date().setDate(new Date().getDate()-10),
+//     exercises: [
+//       {
+//         type: "resistance",
+//         name: "Bicep Curl",
+//         duration: 20,
+//         weight: 100,
+//         reps: 10,
+//         sets: 4
+//       }
+//     ]
+//   },
+//   {
+//     day: new Date().setDate(new Date().getDate()-9),
+//     exercises: [
+//       {
+//         type: "resistance",
+//         name: "Lateral Pull",
+//         duration: 20,
+//         weight: 300,
+//         reps: 10,
+//         sets: 4
+//       }
+//     ]
+//   },
+//   {
+//     day: new Date().setDate(new Date().getDate()-8),
+//     exercises: [
+//       {
+//         type: "resistance",
+//         name: "Push Press",
+//         duration: 25,
+//         weight: 185,
+//         reps: 8,
+//         sets: 4
+//       }
+//     ]
+//   },
+//   {
+//     day: new Date().setDate(new Date().getDate()-7),
+//     exercises: [
+//       {
+//         type: "cardio",
+//         name: "Running",
+//         duration: 25,
+//         distance: 4
+//       }
+//     ]
+//   },
+//   {
+//     day: new Date().setDate(new Date().getDate()-6),
+//     exercises: [
+//       {
+//         type: "resistance",
+//         name: "Bench Press",
+//         duration: 20,
+//         weight: 285,
+//         reps: 10,
+//         sets: 4
+//       }
+//     ]
+//   },
+//   {
+//     day: new Date().setDate(new Date().getDate()-5),
+//     exercises: [
+//       {
+//         type: "resistance",
+//         name: "Bench Press",
+//         duration: 20,
+//         weight: 300,
+//         reps: 10,
+//         sets: 4
+//       }
+//     ]
+//   },
+//   {
+//     day: new Date().setDate(new Date().getDate()-4),
+//     exercises: [
+//       {
+//         type: "resistance",
+//         name: "Quad Press",
+//         duration: 30,
+//         weight: 300,
+//         reps: 10,
+//         sets: 4
+//       }
+//     ]
+//   },
+//   {
+//     day: new Date().setDate(new Date().getDate()-3),
+//     exercises: [
+//       {
+//         type: "resistance",
+//         name: "Bench Press",
+//         duration: 20,
+//         weight: 300,
+//         reps: 10,
+//         sets: 4
+//       }
+//     ]
+//   },
+//   {
+//     day: new Date().setDate(new Date().getDate()-2),
+//     exercises: [
+//       {
+//         type: "resistance",
+//         name: "Military Press",
+//         duration: 20,
+//         weight: 300,
+//         reps: 10,
+//         sets: 4
+//       }
+//     ]
+//   },
+//   {
+//     day: new Date().setDate(new Date().getDate()-1),
+//     exercises: [
+//       {
+//         type: "resistance",
+//         name: "Bench",
+//         duration: 30,
+//         distance: 2
+//       }
+//     ]
+//   }
